555e7d6a7de4ff8216d78a0924b8959f90e06367
Bensyour Ryan <ryan.bensyour@gmail.com>
2024-08-07 18:03:12 +0200

    edit bar et modal

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ const openModal1 = (e) => (
     target.style.display = null
     target.removeAttribute('aria-hidden')
     target.setAttribute('aria-model','true')
+    modal = target
+    modal.addEventListener('click', closemadl)
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
+
 )
+
+const openModal1 = (e) => {
+    if (modal === null) return
+    e.preventDefault()
+    modal.style.display = "none"
+    modal.setAttribute('aria-hidden','true')
+    modal.removeAttribute('aria-model')
+    modal.removeEventListener('click', closemadl)
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
+    modal === null
+
+}
 
 
 document.querySelectorAll('.js-modal').forEach (a => {
